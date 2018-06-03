51e673d82e68904147c38bb9b0bb4c843196d657
usiss <jakabattilaa@gmail.com>
2018-06-03 20:53:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
+   define('DB_SERVER', '');
+   define('DB_USERNAME', '');
    define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'fogadas');
+   define('DB_DATABASE', '');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   
-  mysql_set_charset('utf8_hungarian_ci');
-
 ?>
